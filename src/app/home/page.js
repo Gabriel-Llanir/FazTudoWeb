0c784d3c8dbde83742c016efb22f58312b0f608a
Gabriel Llanir <104911512+InsaneXurow10@users.noreply.github.com>
2023-08-29 10:07:42 -0300

    Create page.js

---
+++ src/app/home/page.js
@@ -0,0 +1,14 @@
+import NavBar from "@/components/NavBar";
+
+
+export default function Categorias() {
+  return (
+    <>
+      <NavBar active={"home"} />
+
+      <main className="bg-slate-900 m-12 p-4 rounded">
+        <h2>Home</h2>
+      </main>
+    </>
+  )
+}
