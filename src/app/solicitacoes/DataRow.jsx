3a858978d82ea04c7fe0f8b535e3b5a656b9e296
Gabriel Llanir <104911512+Gabriel-Llanir@users.noreply.github.com>
2023-09-06 08:18:47 -0300

    Update DataRow.jsx

--- src/app/solicitacoes/DataRow.jsx
+++ src/app/solicitacoes/DataRow.jsx
@@ -1,12 +1,12 @@
 import DropMenu from "@/components/DropMenu";
 import { CreditCardIcon } from "@heroicons/react/24/outline";
 
-export default function DataRow({conta}){
+export default function DataRow({solicitacao}){
     return (
         <div id="data-row" className="flex items-center justify-between hover:bg-slate-800 p-2 rounded cursor-pointer">
             <div className="flex gap-2">
               <CreditCardIcon className="h-6 w-6" />
-              <span>{conta.nome}</span>
+              <span>{solicitacao.nome}</span>
             </div>
             <div className="flex gap-2 items-center">
               <span>R$100.00</span>
