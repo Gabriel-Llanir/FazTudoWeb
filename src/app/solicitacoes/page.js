b9550465e16699ee5dbf56380df633ff36981958
Gabriel Llanir <104911512+Gabriel-Llanir@users.noreply.github.com>
2023-09-06 08:17:48 -0300

    Update page.js

--- src/app/solicitacoes/page.js
+++ src/app/solicitacoes/page.js
@@ -3,32 +3,32 @@ import DataRow from "./DataRow";
 import Button from "@/components/Button";
 import { PlusIcon } from "@heroicons/react/24/outline";
 
-async function getContas() {
-  const url = "http://localhost:8080/api/contas"
+async function getSolicitacoes() {
+  const url = "http://localhost:8080/api/solicitacoes"
   const resp = await fetch(url)
   return resp.json()
 }
 
-export default async function Contas() {
-  const data = await getContas()
+export default async function Solicitacoes() {
+  const data = await getSolicitacoes()
 
   return (
     <>
-      <NavBar active={"contas"} />
+      <NavBar active={"solicitacoes"} />
 
       <main className="bg-slate-900 m-12 p-4 rounded">
         <div className="flex justify-between items-center">
-          <h2 className="text-2xl font-bold">Contas</h2>
+          <h2 className="text-2xl font-bold">Solicitacoes</h2>
           <Button 
             icon={<PlusIcon className="h6 w-6" />}
-            href="/contas/form"
+            href="/solicitacoes/form"
           >
-            criar conta
+            criar solicitação
           </Button>
         </div>
 
         <div id="data">
-          {data.map(conta => <DataRow conta={conta} />)}
+          {data.map(solicitacao => <DataRow solicitacao={solicitacao} />)}
         </div>
       </main>
     </>
