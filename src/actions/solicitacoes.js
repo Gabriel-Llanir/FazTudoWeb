6bd439d63d42b20d6db8db8b41f9fa48de2e98be
Gabriel Llanir <104911512+Gabriel-Llanir@users.noreply.github.com>
2023-09-29 14:35:06 -0300

    jwt

--- src/actions/solicitacoes.js
+++ src/actions/solicitacoes.js
@@ -1,15 +1,18 @@
 "use server"
 
 import { revalidatePath } from "next/cache"
+import { cookies } from 'next/headers'
 
-const url = process.env.NEXT_PUBLIC_BASE_URL +  "/publicacoes"
+const url = process.env.NEXT_PUBLIC_BASE_URL +  "/solicitacoes"
 
 export async function create(formData){
+    const token = cookies().get("faztudo_token")
     const options = {
         method: "POST",
         body: JSON.stringify(Object.fromEntries(formData)),
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token.value}`
         }
     }
     const resp = await fetch(url, options)
@@ -17,7 +20,7 @@ export async function create(formData){
         return {message: "Erro ao cadastrar"}
     }
     
-    revalidatePath("/publicacoes")
+    revalidatePath("/solicitacoes")
     return {message: "ok"}
        
 }
@@ -25,4 +28,46 @@ export async function create(formData){
 export async function getSolicitacoes() {
     const resp = await fetch(url)
     return resp.json()
-  }
+}
+
+export async function apagar(id){
+    const deleteUrl = url + "/" + id
+
+    const options = {
+        method: "DELETE"
+    }
+
+    const resp = await fetch(deleteUrl, options)
+
+    if (resp.status !== 204) return {error: "Erro ao apagar solicitacoes. "}
+
+    revalidatePath("/solicitacoes")
+}
+
+export async function getSolicitacoes(id){
+    const getUrl =  url + "/" + id
+    const resp = await fetch(getUrl)
+
+    if (resp.status !== 200) return {error: "Erro ao carregar dados"}
+
+    return await resp.json()
+
+}
+
+export async function update(solicitacao){
+    const updateUrl =  url + "/" + solicitacao.id
+
+    const options = {
+        method: "PUT",
+        body: JSON.stringify(solicitacao),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    const resp = await fetch(updateUrl, options)
+
+    if (resp.status !== 200) return {error: "Erro ao atualizar solicitação"}
+
+    revalidatePath("/solicitacoes")
+}
