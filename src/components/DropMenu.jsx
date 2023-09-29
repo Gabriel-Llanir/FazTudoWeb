560dd430b851ed4793ac98038663db4b13fd85fd
Gabriel Llanir <104911512+Gabriel-Llanir@users.noreply.github.com>
2023-09-29 15:12:56 -0300

    jwt

--- src/components/DropMenu.jsx
+++ src/components/DropMenu.jsx
@@ -5,7 +5,7 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
-import { apagar } from '@/actions/publicacoes';
+import { apagar } from '@/actions/solicitacoes';
 import toast from 'react-hot-toast';
 import { useToast } from '@/hooks/toast';
 
@@ -38,7 +38,7 @@ export default function DropMenu({ idConta }) {
   };
 
   const handleEdit = () => {
-    push(`/publicacoes/${idSolicitacao}/edit`)
+    push(`/solicitacoes/${idSolicitacao}/edit`)
   }
 
   const handleDelete = async () => {
