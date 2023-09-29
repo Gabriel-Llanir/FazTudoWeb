3409845042bff271e63fb92441492d1ddf44504c
Gabriel Llanir <104911512+Gabriel-Llanir@users.noreply.github.com>
2023-09-29 14:26:13 -0300

    jwt

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,23 @@
 import { Toaster } from 'react-hot-toast'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { AuthProvider } from '@/contexts/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Não Falindo',
-  description: 'Um app para controle de finanças pessoais',
+  title: 'FazTudo',
+  description: 'Um app para solicitação de serviços para profissionais',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        {children}
-        <Toaster position="bottom-right"/>
+        <AuthProvider>
+          {children}
+          <Toaster position="bottom-right"/>
+        </AuthProvider>
       </body>
     </html>
   )
