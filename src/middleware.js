3cbd92408b8ef518398a13e6983902694d2cd687
Gabriel Llanir <104911512+Gabriel-Llanir@users.noreply.github.com>
2023-09-29 13:57:32 -0300

    jwt

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,13 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-    if(!request.cookies.has("naofalindo_token")){
+    if(!request.cookies.has("faztudo_token")){
         return NextResponse.redirect(new URL('/login', request.url))
     }
 }
 
 export const config = {
   matcher: [
-    '/contas/:path*',
-    '/categorias/:path*',
-    '/despesas/:path*',
+    '/solicitacoes/:path*',
   ]
 }
