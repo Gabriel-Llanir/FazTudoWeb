8e7d5c72c37b3919a17293d550c37230ac9877cf
Gabriel Omar <g4br13l.llanir@gmail.com>
2023-09-30 14:19:23 -0300

    Final

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFound() {
         <main className="bg-slate-900 m-12 p-4 rounded">
             <h2>Página não encontrada</h2>
             <br/>
-            <Link href="/">Voltar para Home</Link>
+            <Link href="/">Home</Link>
         </main>
     )
 }
